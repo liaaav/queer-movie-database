0b320cde4f2f966a1d5143c7755fff5ab0dd54a4
liaaav <90356115+liaaav@users.noreply.github.com>
2022-08-22 12:32:53 +1200

    Update movie.php

--- movie.php
+++ movie.php
@@ -1,27 +1,24 @@
 <?php
-    /*Connection to database code*/
-    $con = mysqli_connect("localhost", "liav", "swifthelp20", "liav_queer");
-    if(mysqli_connect_errno()){
-        echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
-    else{
-        echo "connected to database";
-    }
-    if(isset($_GET['item'])){
-        $id = $_GET['item'];
-    }else{
-        $id = "HSW";
-    }
-    date_default_timezone_set('Pacific/Auckland');
-    $day = (strftime("%A"));
-    $today_special_query = "SELECT ItemID FROM specials WHERE Day = '" . $day . "'";
-    $today_special_result = mysqli_query($con, $today_special_query);
-    $today_special_record = mysqli_fetch_assoc($today_special_result);
+/*Connection to database code*/
+$con = mysqli_connect("localhost", "liav", "swifthelp20", "liav_queer");
+if(mysqli_connect_errno()){
+    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+else{
+    echo "connected to database";
+}
+if(isset($_GET['movie'])){
+    $id = $_GET['movie'];
+}else{
+    $id = "1";
+}
 
-    $this_item_query = "SELECT items.*, categories.Category FROM items, categories WHERE ItemID = '" . $id ."'";
-    $this_item_result = mysqli_query($con, $this_item_query);
-    $this_item_record = mysqli_fetch_assoc($this_item_result);
+$this_movie_query = "SELECT movie.* FROM movie WHERE movie_id = '" . $id ."'";
+$this_movie_result = mysqli_query($con, $this_movie_query);
+$this_movie_record = mysqli_fetch_assoc($this_movie_result);
 
-
+$genre_query = "SELECT genre.genre FROM genre, movie, movie_genre WHERE movie.movie_id = '" . $id ."'
+                AND genre.genre_id = movie_genre.genre_id AND movie.movie_id = movie_genre.movie_id";
+$genre_result = mysqli_query($con, $genre_query);
 ?>
 
 <!DOCTYPE html>
@@ -29,92 +26,53 @@
 <head>
     <title> Queer Movie Database </title>
     <meta charset="utf-8">
-    <link rel='stylesheet' type='text/css' href="">
+    <link rel='stylesheet' type='text/css' href="style.css">
+    <!--    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">-->
 </head>
 <body>
-    <div id="page-container">
-        <header>
-            <!-- logo -->
-            <a href = "index.php"><img class="logo" src= "images/WGC_Logo.png" alt="Wellington Girls College Logo"></a>
-            <nav>
-                <div class="navbar">
-                    <div class="navbar-container fr">
-                        <!-- nav links -->
-                        <a href="index.php">Home</a>
-                        <a href="browse.php">Browse</a>
-                        <a href="specials.php">Specials</a>
-                        <!-- search bar -->
-                        <div class = "boxContainer">
-                            <table class="elementsContainer">
-                                <form action="browse.php" method="post">
-                                    <tr>
-                                        <td>
-                                            <input type="text" placeholder="Search"
-                                                   class="search" name="search">
-                                        </td>
-                                        <td>
-                                            <button type="submit" name="submit" value="Search">
-                                                <span class="material-icons-outlined">search</span>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </form>
-                            </table>
-                        </div>
-                    </div>
+<header>
+    <nav>
+        <a class="button" href="index.php">Home</a>
+        <a class="button" href="movies.php">Movies</a>
+        <a class="button" href="about_us.php">About Us</a>
+        <!-- search bar -->
+        <table>
+            <form action="search.php" method="post">
+                <tr>
+                    <td>
+                        <input type="text" placeholder="Search"
+                               class="search" name="search">
+                    </td>
+                    <td>
+                        <button type="submit" name="submit" value="Search">
+                            <span class="material-icons-outlined">search</span>
+                        </button>
+                    </td>
+                </tr>
+            </form>
+        </table>
+    </nav>
+</header>
 
+    <main>
+            <br>
+            <?php
+            echo "<h2>" . $this_movie_record['movie_name'] . "</h2><br>";
+            //            Retrieve information from database of selected movie
 
-                </div>
+            echo "<img class='item-img' src='images/Rainbow_Flag.png' width = '25%'>";
 
-            </nav>
-        </header>
-        <main>
-            <div>
-                <div class='item-information'>
-                    <br>
-                        <h2> ITEM INFORMATION</h2>
-                        <?php
-            //            Retrieve information from database of selected item
+            echo "<p> Representation: " . $this_movie_record['representation'] . "<br>";
+            echo "<p>";
+            while ($genre_record = mysqli_fetch_assoc($genre_result)) {
+                echo $genre_record['genre'];
+                echo ", ";
+            }
+            ?>
+    </main>
 
-                        echo "<img class='item-information-image' src='images/" .$this_item_record['Image'] ."' alt='" . $this_item_record['ItemName'] . "'>";
-                        if($today_special_record['ItemID'] == ($this_item_record['ItemID'])){
-                            echo "<p><strong>TODAY'S SPECIAL! </strong>";
-
-                        }else{
-                            if($this_item_record['Availability'] == 'no'){
-                                echo "<p> NOT AVAILABLE<br>";
-                            }elseif($this_item_record['Availability'] == 'yes') {
-                                echo "<p> IN STOCK <p>";
-                            }
-                        }
-                        echo "<p> Item Name: " . $this_item_record['ItemName'] . "<br>";
-                        echo "<p> Cost: $" . $this_item_record['Cost'] . "<br>";
-                        echo "<p> Category: " . $this_item_record['Category'] . "<br>";
-
-                        if($this_item_record['MeatFree'] == True){
-                            echo "<p> Meat Free<br>";
-                        }
-                        if($this_item_record['DairyFree'] == True){
-                            echo "<p> Dairy Free<br>";
-                        }
-                        if($this_item_record['Vegan'] == True){
-                            echo "<p> Vegan <br>";
-                        }
-                        if($this_item_record['GlutenFree'] == True){
-                            echo "<p> Gluten Free<br>";
-                        }
-
-
-                        ?>
-                </div>
-        </main>
-    </div>
-
-    <footer>
-        <div class="legal-bar">
-            <p>© WGC Ava Li 2021 - all rights reserved </p>
-        </div>
-    </footer>
+<footer>
+</footer>
 </body>
 </html>
 
